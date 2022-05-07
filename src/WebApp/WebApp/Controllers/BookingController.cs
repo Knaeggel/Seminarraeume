1a90ebd66fdc12ac45f8774dedd8a6925bebe9e3
Kai <k.penazzi@web.de>
2022-05-07 13:56:25 +0200

    creating bookingController + some methods

---
+++ src/WebApp/WebApp/Controllers/BookingController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    public class BookingController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public BookingController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ticketing()
+        {
+            var rooms = context.Rooms.toList();
+            ViewBag.Rooms = rooms;
+
+            return View();
+        }
+
+        public IActionResult Overview()
+        {
+            var tickets = context.Tickets.toList();
+            ViewBag.Tickets = tickets;
+
+            return View();
+        }
+    }
+}
