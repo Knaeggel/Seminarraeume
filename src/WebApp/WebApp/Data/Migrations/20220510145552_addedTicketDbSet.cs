3f7a035ffdacfbf69f926f665c7616fc310305e7
Kai <k.penazzi@web.de>
2022-05-10 16:58:04 +0200

    add ticket database

---
+++ src/WebApp/WebApp/Data/Migrations/20220510145552_addedTicketDbSet.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Data.Migrations
+{
+    public partial class addedTicketDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ticktes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    room = table.Column<int>(type: "int", nullable: false),
+                    user = table.Column<int>(type: "int", nullable: false),
+                    block = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticktes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticktes");
+        }
+    }
+}
---
+++ src/WebApp/WebApp/Models/Ticket.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp.Models
+{
+
+
+    public class Ticket
+    {
+        public int id { get; set; }
+
+        public int room { get; set; }
+        public int user { get; set; }
+        public int block { get; set; }
+
+
+
+    }
+}
