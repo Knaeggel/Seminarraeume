b3ef9cc4c32711d55b4ca3a7e59e311878a13e65
KaiPenazzi <k.penazzi@web.de>
2022-05-20 16:12:38 +0200

    add dummyRoles

---
+++ src/WebApp/WebApp/Dummy/DummyRoles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Dummy
+{
+    public class DummyRoles
+    {
+        string[] roles;
+
+        public DummyRoles()
+        {
+            roles = new string[]
+            {
+                "Student",
+                "Prof"
+            };
+        }
+
+        public async void FillDummy(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            var roleExists = await roleManager.RoleExistsAsync("Student");
+        }
+    }
+}
---
+++ src/WebApp/WebApp/Dummy/DummyUsers.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Dummy
+{
+    public class DummyUsers
+    {
+        public async void FillDummy(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            for (int i = 1; i < 51; i++)
+            {
+                var user = await userManager.FindByNameAsync("User" + i + "@proOne.de");
+
+                if (user == null)
+                {
+                    var newUser = new IdentityUser()
+                    {
+                        UserName = "User" + i,
+                        Email = "User" + i + "@proOne.de"
+                    };
+
+                    await userManager.CreateAsync(newUser, "Test" + i);
+
+                    user = await userManager.FindByNameAsync("User" + i + "@proOne.de");
+                }
+
+                await userManager.AddToRoleAsync(user, "Student");
+            }
+        }
+    }
+}
