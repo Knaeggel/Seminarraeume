28adebdab7f6b34ee52aee9910d52097586d4c4b
Your Name <you@example.com>
2022-05-20 15:01:46 +0200

    Delete user

--- src/WebApp/WebApp/Data/ApplicationDbContext.cs
+++ src/WebApp/WebApp/Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace WebApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<User> Nutzer { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Ticket> Ticktes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
