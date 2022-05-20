f7e157a2ce02d8d86626df13d761cad889dbe91a
Your Name <you@example.com>
2022-05-20 18:29:50 +0200

    Dummy

--- src/WebApp/WebApp/Dummy/DummyRooms.cs
+++ src/WebApp/WebApp/Dummy/DummyRooms.cs
@@ -17,21 +17,59 @@ namespace WebApp.Dummy
         public void FillDummy(ApplicationDbContext dbSet)
         {
 
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Room room = new Room();
-                room.RoomName = "A"+i;
-                room.KindOfRoom = "Labor";
+                room.RoomName = "A10" + ( i + 1);
+                room.KindOfRoom = "Vorlesungssaal";
                 room.Status = false;
-                room.RoomSize = 20;
-                dbSet.Rooms.Add(room);
-                dbSet.SaveChanges();
+                room.RoomSize = 30;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
             }
 
+            for (int i = 0; i < 5; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "A10" + (i + 1);
+                room.KindOfRoom = "Labor";
+                room.Status = false;
+                room.RoomSize = 30;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+
+
+
+
+
+
         }
-
-
-
 
     }
 }
