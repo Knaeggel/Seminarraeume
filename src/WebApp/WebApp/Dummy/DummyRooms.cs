15986f6900138172a6598ca0dafad5aa181bf324
noezcay2 <nec.oezcay@hotmail.de>
2022-05-20 18:49:30 +0200

    Räume gefüllt mit Dummy daten

--- src/WebApp/WebApp/Dummy/DummyRooms.cs
+++ src/WebApp/WebApp/Dummy/DummyRooms.cs
@@ -23,7 +23,7 @@ namespace WebApp.Dummy
                 room.RoomName = "A10" + ( i + 1);
                 room.KindOfRoom = "Vorlesungssaal";
                 room.Status = false;
-                room.RoomSize = 30;
+                room.RoomSize = 25;
                 bool found = false;
                 foreach (var item in dbSet.Rooms.ToList())
                 {
@@ -46,6 +46,31 @@ namespace WebApp.Dummy
                 room.RoomName = "A10" + (i + 1);
                 room.KindOfRoom = "Labor";
                 room.Status = false;
+                room.RoomSize = 10;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "B20" + (i + 1);
+                room.KindOfRoom = "PC-Raum";
+                room.Status = false;
                 room.RoomSize = 30;
                 bool found = false;
                 foreach (var item in dbSet.Rooms.ToList())
@@ -62,6 +87,174 @@ namespace WebApp.Dummy
                     dbSet.SaveChanges();
                 }
             }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "B04" + (i + 1);
+                room.KindOfRoom = "Labor";
+                room.Status = false;
+                room.RoomSize = 55;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+            for (int i = 4; i < 10; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "C10" + (i + 1);
+                room.KindOfRoom = "Vorlesungssaal";
+                room.Status = false;
+                room.RoomSize = 125;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+
+            for (int i = 4; i < 10; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "C00" + (i + 1);
+                room.KindOfRoom = "Vorlesungssaal";
+                room.Status = false;
+                room.RoomSize = 15;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "D00" + (i + 1);
+                room.KindOfRoom = "Medienraum";
+                room.Status = false;
+                room.RoomSize = 23;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+
+            for (int i = 4; i < 10; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "D10" + (i + 1);
+                room.KindOfRoom = "Vorlesungssaal";
+                room.Status = false;
+                room.RoomSize = 60;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+            for (int i = 2; i < 8; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "E00" + (i + 1);
+                room.KindOfRoom = "PC-Raum";
+                room.Status = false;
+                room.RoomSize = 25;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+            for (int i = 1; i < 8; i++)
+            {
+                Room room = new Room();
+                room.RoomName = "E30" + (i + 1);
+                room.KindOfRoom = "Vorlesungssaal";
+                room.Status = false;
+                room.RoomSize = 45;
+                bool found = false;
+                foreach (var item in dbSet.Rooms.ToList())
+                {
+                    if (item.RoomName == room.RoomName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    dbSet.Rooms.Add(room);
+                    dbSet.SaveChanges();
+                }
+            }
+
+
+
+
         }
 
     }
