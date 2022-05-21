0a032d53c627475a4c69f296a58e1ba9b1109857
KaiPenazzi <k.penazzi@web.de>
2022-05-22 00:35:56 +0200

    Password thinks

--- src/WebApp/WebApp/Program.cs
+++ src/WebApp/WebApp/Program.cs
@@ -11,7 +11,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<IdentityUser>(
+    options => {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireDigit = false;
+        })
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
