9290c8776a3174e3feffb59d87f9a9cc8e0b73d3
KaiPenazzi <k.penazzi@web.de>
2022-05-24 14:51:54 +0200

    add better tickets and block.date

---
+++ src/WebApp/WebApp/Data/Migrations/20220523232942_betterTickets.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Data.Migrations
+{
+    public partial class betterTickets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "date",
+                table: "Ticktes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "date",
+                table: "Blocks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "date",
+                table: "Ticktes");
+
+            migrationBuilder.DropColumn(
+                name: "date",
+                table: "Blocks");
+        }
+    }
+}
--- src/WebApp/WebApp/Dummy/DummyTickets.cs
+++ src/WebApp/WebApp/Dummy/DummyTickets.cs
@@ -12,6 +12,7 @@ namespace WebApp.Dummy
 
         public void FillDummy(ApplicationDbContext dbSet)
         {
+            /*
             var tickets = dbSet.Ticktes.ToList();
 
             //create Tickets loop
@@ -78,6 +79,31 @@ namespace WebApp.Dummy
                             dbSet.Blocks.Add(new Block(new DateTime(2022, 10, 15), 1, newTicket.id));
                         }
 
+                    }
+                }
+            }
+            */
+            var random = new Random();
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    var newTicket = new Ticket(random.Next(1, 50), random.Next(1, 70), new DateTime(2022, 7, 27 + j), random.Next(1, 8));
+
+                    var found = false;
+                    foreach (var item in dbSet.Ticktes.ToList())
+                    {
+                        if (newTicket.compare(item))
+                        {
+                            found = true;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        dbSet.Ticktes.Add(newTicket);
+                        dbSet.SaveChanges();
                     }
                 }
             }
