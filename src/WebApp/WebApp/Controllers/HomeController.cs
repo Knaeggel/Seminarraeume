84ded53096ae43f1514eb6595429b4f7b1f457d4
Alex <xandi.kehl@freenet.de>
2022-05-30 18:28:36 +0200

    controller auf paging angepasst

--- src/WebApp/WebApp/Controllers/HomeController.cs
+++ src/WebApp/WebApp/Controllers/HomeController.cs
@@ -56,7 +56,7 @@ namespace WebApp.Controllers
             return View();
         }
 
-        public IActionResult räume(string name)
+        public async Task<IActionResult> räume(string name, int pageNumber = 1)
         {
             var today = new DateTime();
             today = DateTime.Now;
@@ -103,6 +103,7 @@ namespace WebApp.Controllers
             ViewBag.date = today;
             ViewBag.Days = days;
             ViewBag.Room = selectedRoom;
+
             return View(await PaginatedList<Room>.CreateAsync(_context.Rooms, pageNumber, 5));
         }
 
