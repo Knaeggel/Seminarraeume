609efc3269effdf17c5151648ead0cd30bc89305
KaiPenazzi <k.penazzi@web.de>
2022-05-30 17:08:38 +0200

    search for building

--- src/WebApp/WebApp/wwwroot/js/site.js
+++ src/WebApp/WebApp/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$("#Building").change(function() {
+    hideUselessButtons($("#Building")[0].value)
+})
+
+function hideUselessButtons(building) {
+    let collection = $(".rooms")
+
+    if (building == "all") {
+        for (var i = 0; i < collection.length; i++) {
+            collection[i].hidden = false;
+        }
+    }
+    else {
+        for (var i = 0; i < collection.length; i++) {
+            let y = collection[i].innerText.charAt(0)
+            if (collection[i].innerText.trim().charAt(0) == building) {
+                collection[i].hidden = false;
+            }
+            else {
+                collection[i].hidden = true;
+            }
+        }
+    }
+}
