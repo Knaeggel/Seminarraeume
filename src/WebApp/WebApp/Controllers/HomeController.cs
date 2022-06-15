fcfe08fe0275cf3e60c586f090e3195bae4388c9
KaiPenazzi <k.penazzi@web.de>
2022-06-15 18:19:51 +0200

    creat tickets are dissabled

--- src/WebApp/WebApp/Controllers/HomeController.cs
+++ src/WebApp/WebApp/Controllers/HomeController.cs
@@ -33,7 +33,10 @@ namespace WebApp.Controllers
                 var dummyRoles = new DummyRoles(roleMgr);
                 var dummyUsers = new DummyUsers(userMgr);
                 var DummyRooms = new DummyRooms(con);
-                var dummyTickets = new DummyTickets(con, userMgr);
+                if (false)
+                {
+                    var dummyTickets = new DummyTickets(con, userMgr);
+                }
                 
             }
 
